a4004203be939b4fd9f9d41bc598dfe1eabdec64
Arulrot <iamarulmohan333@gmail.com>
2024-07-30 18:43:59 +0530

    .

---
+++ python-1.py
@@ -0,0 +1,9 @@
+
+print("Welcome to the Band Name Generator \n")
+
+Name=input("Enter your name : ")
+
+Father_name=input("\nEnter your father name : ")
+
+print("\nYour Family Name is " +Father_name+ "_" +Name)
+    
