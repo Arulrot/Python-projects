05b5d30f8b8517291f620f1dccab504101fdaa26
arulrot <iamarulmohan333@gmail.com>
2024-08-10 23:44:41 +0530

    .

--- Auction_platform.py
+++ Auction_platform.py
@@ -1,21 +1,27 @@
 emptdict={}
 def higgestbid():
-    highestbidder=''
+    winner=''
     hieghestbid=0
-    for bidder in empdict:
-        bidamount=empdict[bidder]
+    for bidder in emptdict:
+        bidamount=emptdict[bidder]
         if bidamount>hieghestbid:
+            hieghestbid=bidamount
+            winner=bidder
+    print(f'\nThe winner is {winner} with the bid amount of {hieghestbid}')
             
 
 while True:   
    
-    name=input("enter your name:")
-    value=int(input("enter amount:"))
+    name=input("\nEnter your name:")
+    value=int(input("\nEnter amount:"))
     emptdict[name]=value
-    opt=input("Any other bids 'yes' or 'no':").lower()
+    opt=input("\nAny other bids 'yes' or 'no':").lower()
     if opt=='yes':
         continue;
+    elif opt=='no':
+        higgestbid()
+        break;
     else :
+        print("\nWrong input")
         break;
         
-print(emptdict)
